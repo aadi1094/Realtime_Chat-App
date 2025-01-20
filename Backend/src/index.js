4c7a339556aa275470744236ec2e71c4db14afef
CHAWALE ADITYA SUDHAKAR <adityachawale14@gmail.com>
2025-01-20 16:49:43 +0530

    Project Done

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -13,7 +13,7 @@ dotenv.config()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:process.env.FRONTEND_URL || "http://localhost:5173",
     credentials:true,
     methods:["OPTIONS","OPTION","GET","POST","PUT","DELETE"]
 }))
