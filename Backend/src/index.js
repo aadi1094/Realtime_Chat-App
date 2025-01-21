d652c1ad0eac592a9349a61ec3b37b876b3174a1
CHAWALE ADITYA SUDHAKAR <adityachawale14@gmail.com>
2025-01-21 12:26:05 +0530

    Project Completed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -28,7 +28,7 @@ app.use("/api/message",messageRoute)
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"../Frontend/dist")))
 
-    app.length("*",(req,res)=>{
+    app.get("*",(req,res)=>{
         res.sendFile(path.join(__dirname,"../Fronend","dist","index.html"))
     })
 }
